61b6d25fd544486fff27f083433df8506224f21b
Sudhanshu <shudhanshukaprowan@gmail.com>
2024-05-31 14:52:03 +0530

    array

--- variables.php
+++ variables.php
@@ -3,5 +3,7 @@
 $name="harry";
 $income=30000000;
 echo "Employee : $name Salary : $income <br>";
-echo "this person $name is a male";
+echo "this person $name is a male <br>";
+$friend = array("shubham","rohan","mohan","nigga_boi69");
+echo var_dump($friend);
 ?>
